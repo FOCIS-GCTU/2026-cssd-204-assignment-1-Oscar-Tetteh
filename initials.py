eb961167d204e651da624beafaf343082ce720a5
Oscar-Tetteh <2425402292@live.gctu.edu.gh>
2026-05-31 17:40:56 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Oscar Tetteh
+# STUDENT ID: 2425402292
+# Email: 2425402292@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Oscar Tetteh, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Print my initials MOT in small form and as large stylized block letters.
+    print()
+    print("...MOT")
+    print()
+    print("...MM........MM............OOOO............TTTTTTTTTTTT.....")
+    print("...MMM......MMM..........OO....OO..........TTTTTTTTTTTT.....")
+    print("...MMMM....MMMM.........OO......OO..............TT..........")
+    print("...MM.MM..MM.MM........OO........OO.............TT..........")
+    print("...MM..MMMM..MM........OO........OO.............TT..........")
+    print("...MM...MM...MM........OO........OO.............TT..........")
+    print("...MM........MM........OO........OO.............TT..........")
+    print("...MM........MM........OO........OO.............TT..........")
+    print("...MM........MM...**....OO......OO....**........TT........**")
+    print("...MM........MM...**.......OOOO.......**........TT........**")
+    print()
 
 
 main()
